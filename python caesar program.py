b933542cf3b13f17efdc09f1357e0d5cf0e477c9
mugomwaniki <mwanikimugop7@gmail.com>
2023-07-03 15:12:57 +0300

    python caeser program

---
+++ python caesar program.py
@@ -0,0 +1,83 @@
+letters ='abcdefgijklmnopqrstuvwxyz'
+num_letters = len(letters)
+
+#def encrypt(plaintext,key):
+ #   ciphertext =''
+  #  for letter in plaintext:
+   #     letter = letter.lower()
+    #    if not letter == ' ':
+           # index = letters.find(letter)
+           # if index == -1:
+            #    ciphertext += letter
+           # else:
+             #  new_index = index +  key
+             #  if new_index >= num_letters:
+            #     new_index -= num_letters
+           # ciphertext += letters[new_index]
+   # return ciphertext
+#def decrypt(ciphertext,key):
+    #plaintext =''
+    #for letter in ciphertext:
+        #letter = letter.lower()
+        #if not letter == ' ':
+       #     index = letters.find(letter)
+       #     if index == -1:
+      #          plaintext += letter
+      #      else:
+     #          new_index = index - key
+     #          if new_index < 0:
+     #            new_index += num_letters 
+    #        plaintext += letters[new_index]
+    #return plaintext
+
+def encrypt_decrypt(text, mode, key):
+        result = ' '
+        if mode =='D':
+            key = -key
+
+        for letter in text:
+            letter = letter.lower()
+            if not letter ==' ':
+                index = letters.find(letter)
+                if index == -1:
+                    result += letter
+                else:
+                    new_index = index = key
+                    if new_index >= num_letters:
+                        new_index -= num_letters
+                    elif new_index < 0:
+                        new_index += num_letters
+                    result += letters[new_index]
+        return result                  
+
+
+
+
+
+
+
+
+print()
+print( 'Decrypting and encrypting a caesar file,What would you like?')
+user_input = input('E/D ').lower()
+print()
+
+if user_input=='e':
+    print('ENCRYPTION MODE')
+    print()
+    key =int (input('Enter the key to encrypt( 1 through 26):'))
+    text = input('Enter the text to encrypt')
+    ciphertext= encrypt_decrypt(text, user_input, key)
+    print(f'CIPHERTEXT: {ciphertext}')
+
+elif user_input == 'd':
+    print('DECRYPTION MODE')
+    print()
+    key = int(input('Enter the key to decrypt( 1 through 26):'))
+    text = input('Enter the text to decrypt')
+    plaintext= encrypt_decrypt(text,user_input, key)
+    print(f'PLAINTEXT: {plaintext}')
+
+
+
+
